cd7138108cedabb589c6de82efb454e54f98359e
Vyas Mankar <vyas.mankar44@gmail.com>
2016-08-10 15:47:01 +0530

    create adapter and layouts

---
+++ app/src/main/java/com/tabeeby/doctor/adapter/FindDoctorAdapter.java
@@ -0,0 +1,52 @@
+package com.tabeeby.doctor.adapter;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tabeeby.doctor.R;
+
+import java.util.ArrayList;
+
+public class FindDoctorAdapter extends RecyclerView.Adapter<FindDoctorAdapter.ListHolder> {
+
+    private Context mContext;
+    private static ArrayList<String> items;
+
+    public static class ListHolder extends RecyclerView.ViewHolder {
+
+
+        public ListHolder(View itemView) {
+            super(itemView);
+            }
+
+        }
+
+        public FindDoctorAdapter(Context context, ArrayList<String> arrayList){
+            mContext = context;
+            this.items = arrayList;
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+        @Override
+        public ListHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.patient_card, viewGroup, false);
+            ListHolder listHolder = new ListHolder(v);
+            return listHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ListHolder ListHolder, final int i) {
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
+    }
--- app/src/main/java/com/tabeeby/doctor/fragments/AppointmentsFragment.java
+++ app/src/main/java/com/tabeeby/doctor/fragments/AppointmentsFragment.java
@@ -3,21 +3,61 @@ package com.tabeeby.doctor.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.tabeeby.doctor.R;
+import com.tabeeby.doctor.adapter.AppointmentAdapter;
+import com.tabeeby.doctor.adapter.FindDoctorAdapter;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Z510 on 8/4/2016.
  */
 public class AppointmentsFragment extends Fragment {
 
+    ArrayList<String> arrayList;
+    AppointmentAdapter findDoctorAdapter;
+    LinearLayoutManager linearLayoutManager;
+/*    @Bind(R.id.lv_appointment_list)
+    protected RecyclerView mRvListView;*/
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.appointment_layout, container, false);
+       // ButterKnife.bind(getActivity());
         return view;
     }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView recyclerView = (RecyclerView) getActivity().findViewById(R.id.lv_appointment_list);
+        arrayList=new ArrayList<>();
+
+        //this is for six cards
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+        arrayList.add("4");
+        arrayList.add("5");
+        arrayList.add("6");
+
+        findDoctorAdapter = new AppointmentAdapter(getActivity(),arrayList);
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(findDoctorAdapter);
+    }
+
 }
