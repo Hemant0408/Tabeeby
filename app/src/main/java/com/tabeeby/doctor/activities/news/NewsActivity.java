d7eedf49e23d4cca87827582834978eb881c80d4
Vyas Mankar <vyas.mankar44@gmail.com>
2016-08-11 15:18:15 +0530

    Create event and news screens and adapter

---
+++ app/src/main/java/com/tabeeby/doctor/activities/news/NewsActivity.java
@@ -0,0 +1,74 @@
+package com.tabeeby.doctor.activities.news;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.tabeeby.doctor.R;
+import com.tabeeby.doctor.adapter.AppointmentAdapter;
+import com.tabeeby.doctor.adapter.NewsAdapter;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class NewsActivity extends AppCompatActivity {
+    ArrayList<String> arrayList;
+    NewsAdapter findDoctorAdapter;
+    LinearLayoutManager linearLayoutManager;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+        ButterKnife.bind(this);
+
+        setUpActionBar();
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.lv_news_list);
+        arrayList = new ArrayList<>();
+
+        //this is for six cards
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+        arrayList.add("4");
+        arrayList.add("5");
+        arrayList.add("6");
+
+        findDoctorAdapter = new NewsAdapter(this, arrayList);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(findDoctorAdapter);
+    }
+
+    private void setUpActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setElevation(0.2f);
+        getSupportActionBar().setTitle("News");
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+}
